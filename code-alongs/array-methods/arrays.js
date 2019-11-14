28e3b656cf1b6d0e402a54c939d093b2690ab230
BJ Clark <bjclark13@outlook.com>
2019-11-14 18:06:47 -0500

    Add a bunch of instructions for array ethods

---
+++ code-alongs/array-methods/arrays.js
@@ -0,0 +1,32 @@
+// Use built-in array methods to make the following changes
+let myArray = [1,4,2,7,4,345,732423,134,56];
+
+// Sort the array from smallest to biggest
+
+// Add the number 12 to the end array
+
+// Remove 732423 from the array
+
+// Remove the last item from the array and console it out
+
+// Reverse the array
+
+// Find out if the array includes the number 8
+
+// Find the first item in the array that is greater than 18
+
+// Console.log an array made up of only the
+// numbers that are greater than 18
+
+// Remove the first item from the array
+
+// Add 700 to the beginning of the array
+
+// Get the min and max of the numbers in the array
+
+// Get the sum of the numbers in the array
+
+// Convert all of the numbers in the array to strings
+
+
+
