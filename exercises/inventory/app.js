6266f2b3f14697b2acec9726b22398134894202e
BJ Clark <bjclark13@outlook.com>
2019-11-20 20:00:27 -0500

    Add starting place

--- exercises/inventory/app.js
+++ exercises/inventory/app.js
@@ -2,39 +2,40 @@ class Store {
     constructor() {
         // Array of Objects
         this.inventory = [
-
+            new Computer('ASUS', 350),
+            new Computer('Dell', 250),
+            new Laptop('Macbook Pro', 250000),
+            new Laptop('Chromebook', 27.12),
+            new Sticker('Spiderman', 12),
+            new HDMI('Cheap Chinese Knockoff', 2),
+            new HDMI('Official Stuff', 70),
+            { name: 'ASUS', price: 70 }
         ];
     }
 
     addProduct = product => {
-        this.inventory.push(product);
+        return this.inventory.push(product);
     }
 
-    removeProduct = (product) => {
+    removeProduct = (productNumber) => {
+        this.inventory.splice(productNumber, 1);
+    }
 
+    getProductsByType(type) {
+        return this.inventory.filter( item ->item instanceof type );
     }
 
     // Get an array of all of the computer objects
-    getComputers() {
-        this.inventory.filter( (item) => {
-            if ( isComputer ) {
-                return true;
-            } else {
-                return false;
-            }
-        })
-    }
+    getComputers = _ => this.getProductsByType(Computer);
 
     // Get an array of all of the laptop objects
-    getLaptops() {
-
-    }
+    getLaptops = _ => this.getProductsByType(Laptop);
 }
 
 class Product {
-    constructor() {
-        this.name = "Generic Product";
-        this.price = 1;
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
         this.type = 'product';
     }
 
@@ -81,7 +82,13 @@ let dell = new Laptop();
 // console.log(dell instanceof Product);
 
 let store = new Store();
+
 // add products
+let productIndex = store.addProduct(new Monitor('AlienWare', 30));
+console.log(store);
+
+store.addProduct(dell);
+
 
 // get all laptops
 
