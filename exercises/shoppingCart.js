d485b4e8ded02a0828fbb5fac47cd5263360b2d4
BJ Clark <bjclark13@outlook.com>
2019-12-02 20:45:57 -0500

    Add wishlist code along

--- exercises/shoppingCart.js
+++ exercises/shoppingCart.js
@@ -1,10 +1,4 @@
-const itemList = [
-    new Items('Tiger', 3000),
-    new Items('Laptop', 2000),
-    new Items('Phone', 600),
-    new Items('Gum', 2.50),
-    new Items('Batman Comic', 4.99)
-]
+
 
 class Items {
     constructor(name, price) {
@@ -16,11 +10,36 @@ class Items {
 class ShoppingCart {
     shoppingCart = [];
 
+    constructor(cart) {
+        this.shoppingCart = cart;
+    }
+
     addProduct(product) {
         this.shoppingCart = [...this.shoppingCart, product]
     }
 
     removeItem(name) {
+        this.shoppingCart.splice(this.shoppingCart.findIndex( product => {
+            return name === product.name;
+        }), 1);
+    }
 
+    editItem(product) {
+        this.shoppingCart[this.shoppingCart.findIndex( product => {
+            return name === product.name;
+        }) ] = product;
     }
 }
+
+const itemList = [
+    new Items('Tiger', 3000),
+    new Items('Laptop', 2000),
+    new Items('Phone', 600),
+    new Items('Gum', 2.50),
+    new Items('Batman Comic', 4.99)
+]
+
+let cart = new ShoppingCart(itemList);
+console.log(cart.shoppingCart);
+cart.removeItem('Tiger');
+console.log(cart.shoppingCart);
