786a84cb939e1269e891d366e5caff53d6126b3b
BJ Clark <bjclark13@outlook.com>
2019-12-04 18:05:33 -0500

    Add event demo

---
+++ demos/event-bubbling/index.js
@@ -0,0 +1,18 @@
+"use strict"
+{
+    const ul = document.querySelector('ul');
+    ul.addEventListener( 'click', e => {
+        alert('ul was clicked!!!');
+    });
+
+    for(let i = 0; i < 10; i++) {
+        const li = document.createElement('li');
+        li.innerText = `Item #${i + 1}`;
+
+        li.addEventListener( 'click', e => {
+            alert('li was clicked!!!');
+        });
+
+        ul.appendChild(li);
+    }
+}
